8b4624a5bd1d87a8f6b48c94e4757cb3fe7da2c7
Darryl Blake <darryl@nikordi.com>
2015-07-16 21:51:24 -0700

    Passing tests. All basic funcationality implemented.

--- client/collections/SongQueue.js
+++ client/collections/SongQueue.js
@@ -2,6 +2,26 @@
 var SongQueue = Songs.extend({
 
   initialize: function(){
+    this.on('add', this.enqueue, this);
+    this.on('ended', this.dequeue, this);
+    this.on('dequeue', this.dequeue, this);
+  },
+
+  enqueue: function(song){
+    if( this.length === 1 ){
+      this.playFirst();
+    }
+  },
+
+  dequeue: function(song){
+    this.shift();
+    if (this.length > 0) {
+      this.playFirst();
+    }
+  },
+  
+  playFirst: function(){
+    this.at(0).play();
   }
 
 });
